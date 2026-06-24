8097cea57246046959a89b4a5f8a70a14b9027c5
luiselianch-cell <luiselianch@gmail.com>
2026-06-23 21:48:24 -0600

    fixsearch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@
 import { useState, useEffect } from "react";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, PieChart, Pie, Cell } from "recharts";
 // eslint-disable-next-line no-unused-vars
-import { Search } from "lucide-react";
 import { Home, ClipboardList, BarChart2, Users, Search, Menu } from "lucide-react";
 
 const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
